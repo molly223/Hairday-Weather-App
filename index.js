32942c4fa4e2dce3aa59dad8bc143559a3297c3a
molly223 <whitneymcfadden322@gmail.com>
2024-11-04 11:15:12 -0800

    Updated index.js
    Corrected minutes display to two digits.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function formatDate(date) {
     let day = days[date.getDay()];
 
     if (minutes < 10) {
-        minutes = `0${minutes}`;
+        minutes = `0${minutes}`: minutes;
     }
 
     let period = hours >= 12 ? 'PM' : 'AM';
